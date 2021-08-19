2dac51cc5fcf3e2f49efe367c0669a2cf56380ae
andriikuruch <andriikuruch@gamil.com>
2021-08-19 15:07:07 +0300

    remove apps' template dir, add common template dir, add common static dir

--- lab/settings.py
+++ lab/settings.py
@@ -54,8 +54,8 @@ ROOT_URLCONF = 'lab.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
+        'DIRS': ["templates", ],
+        'APP_DIRS': False,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
@@ -118,6 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
